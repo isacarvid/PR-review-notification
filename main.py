54fd77d3699550ebb91910ed227b968e7ecba090
isacarvid <isac.arvidsson97@gmail.com>
2021-03-29 11:41:38 +0200

    open readme file in repo

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
+    yaml_path = os.environ["INPUT_PATH"]
     my_input = os.environ["INPUT_MYINPUT"]
 
     my_output = f"Hello {my_input}"
+    lines = open("README.md").readlines
+    my_output = lines[0]
+    for i in range(lines):
+        if lines[i] == "##Members":
+            my_output = "yessssss"
 
+
+        
     print(f"::set-output name=myOutput::{my_output}")
 
 
