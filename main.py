82a50ce3eeff9a3746d5daa112eb3264af739d07
isacarvid <isac.arvidsson97@gmail.com>
2021-03-31 13:06:42 +0200

    test

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ def main():
     file_path = os.environ["INPUT_FILEPATH"]
 
     my_output = f"hejhej{file_path}"
-    lines = open("README.md").readlines
+    '''lines = open("README.md").readlines
     my_output = lines[0]
     for i in range(lines):
         if lines[i] == "##Members":
-            my_output = "yessssss" 
+            my_output = "yessssss" '''
+    print("hej")
     print(f"::set-output name=myOutput::{my_output}")
 
 if __name__ == "__main__":
