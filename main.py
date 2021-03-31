dcdc2c196012382a9665cc79611115c90e1a20af
isacarvid <isac.arvidsson97@gmail.com>
2021-03-31 11:52:16 +0200

    test

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ def main():
     yaml_path = os.environ["INPUT_PATH"]
     file_path = os.environ["INPUT_FILEPATH"]
 
-    my_output = f"hejhej"
+    my_output = f"hejhej{file_path}"
     lines = open("README.md").readlines
     my_output = lines[0]
     for i in range(lines):
         if lines[i] == "##Members":
-            my_output = "yessssss"
-
-
-        
-    print(f"::set-output name=myOutput::{"hejhej"}")
-
+            my_output = "yessssss" 
+    print(f"::set-output name=myOutput::{my_output}")
 
 if __name__ == "__main__":
     main()
