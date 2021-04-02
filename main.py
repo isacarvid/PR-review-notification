fa41be28f896e6c3630bddd901223bc7699b4014
a y u b <28533161+ayubatif@users.noreply.github.com>
2021-04-02 14:32:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ def sendEmail(username, password, domain, toAddress, msg):
         server.sendmail(username, toAddress, msg)
 
 
-def emailFinder(readme):
+def emailFinder(readme, domain):
+    addr_suffix = "@"+domain
     num_email = 0
     mails = []
 
     for line in readme:
+        addr_suffix = "@"+domain
         domain_index = 0
         start_index = 0
         while domain_index < len(line):
-            domain_index = line.find('@kth.se', domain_index)
+            domain_index = line.find(addr_suffix, domain_index)
             if domain_index == -1:
                 break
 
@@ -27,13 +29,16 @@ def emailFinder(readme):
             start_index = domain_index
             while len(mails) != num_email and start_index >= 0:
                 if line[start_index] == ' ':
-                    start_index += 1
-                    mails.append(line[start_index:domain_index+6])
+                    mails.append(line[start_index+1:domain_index+len(addr_suffix)])
+                    break
+                if start_index == 0:
+                    mails.append(line[start_index:domain_index+len(addr_suffix)])
                     break
                 start_index -= 1
             
-            domain_index += 7 # 7 is len("@kth.se")
-    return (num_email, mails)
+            domain_index += len(addr_suffix)
+            
+    return mails
 
 def main():
     file_path = os.environ["INPUT_FILEPATH"]
@@ -58,7 +63,8 @@ def main():
             break
 
     if notify:
-        (num_mail, mails) = emailFinder(readme)
+        mails = emailFinder(readme, "kth.se")
+        mails += emailFinder(readme, "gmail.com") # for debug
         for to_addr in mails:
             sendEmail(username, pwd, domain, to_addr, message)
 
