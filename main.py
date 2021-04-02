b902a7f8a5fefb80c1acaec10a50a629064e43d7
isacarvid <isac.arvidsson97@gmail.com>
2021-04-02 10:08:06 +0200

    staching

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import os
+import smtplib, ssl
+
+def sendEmail(username, password):
+    port = 465
+    context = ssl.create_default_context()
+    server = ""
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(username, password)
+
+    return True
 
 def main():
     file_path = os.environ["INPUT_FILEPATH"]
     username = os.environ["INPUT_USERNAMESECRET"]
     pwd = os.environ["INPUT_PASSWORDSECRET"]
-
+    domain = os.environ["INPUT_DOMAINSECRET"]
 
     print(pwd)
-    print(username)
-    my_output = f"hejhej{file_path}"
-    '''lines = open("README.md").readlines
-    my_output = lines[0]
-    for i in range(lines):
-        if lines[i] == "##Members":
-            my_output = "yessssss" '''
+    print(username)  
     print("hej")
     print(file_path.splitlines())
+
     readme = []
     for file in file_path.splitlines():
         if file.find("README") != -1:
